8a8298d4bd87e1e33e5482871bc864cf965da7d3
kotyal <shivagond2001@gmail.com>
2023-03-13 12:01:02 +0530

    new branck kotyal added with add and manage test buttons in tutor home screen

---
+++ app/src/main/java/com/example/loginpage/tutor_/tutor_add_test.java
@@ -0,0 +1,16 @@
+package com.example.loginpage.tutor_;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.loginpage.R;
+
+public class tutor_add_test extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutor_add_test);
+    }
+}
